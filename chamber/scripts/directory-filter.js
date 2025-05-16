e343da9bd85015d8276bf504614cfd8aba66bed8
codejoeylee <benjaminoforiasante@gmail.com>
2025-05-16 15:13:06 +0000

    Chamber of commerce directory page

---
+++ chamber/scripts/directory-filter.js
@@ -0,0 +1,60 @@
+async function fetchMembers() {
+  const response = await fetch("json/members.json");
+  const data = await response.json();
+  displayMembers(data.companies);
+}
+
+const displayMembers = (companies) => {
+  companies.forEach((company) => {
+    let card = document.createElement("section");
+    let fullName = document.createElement("h2");
+    let Email = document.createElement("h5");
+    let Phone = document.createElement("h5");
+    let Website = document.createElement("h5");
+    let portrait = document.createElement("img");
+
+    fullName.textContent = `${company.name}`;
+    Email.textContent = `Email: ${company.email}`;
+    Phone.textContent = `Phone: ${company.phone}`;
+    Website.textContent = `Website: ${company.website}`;
+    portrait.setAttribute("src", company.image);
+    portrait.setAttribute("alt", `Logo of ${company.name}`);
+    portrait.setAttribute("loading", "lazy");
+    portrait.setAttribute("width", "340");
+    portrait.setAttribute("height", "440");
+
+    card.appendChild(fullName);
+    card.appendChild(Email);
+    card.appendChild(Phone);
+    card.appendChild(Website);
+    card.appendChild(portrait);
+
+    document.querySelector("div.cards").appendChild(card);
+  });
+};
+
+fetchMembers();
+
+document.querySelector(".btn1").addEventListener("click", () => {
+  document.querySelector(".cards").classList.remove("list-view");
+  document.querySelector(".cards").classList.add("grid-view");
+});
+
+document.querySelector(".btn2").addEventListener("click", () => {
+  document.querySelector(".cards").classList.remove("grid-view");
+  document.querySelector(".cards").classList.add("list-view");
+});
+
+const year = document.querySelector("#currentyear");
+const today = new Date();
+year.innerHTML = ` ${today.getFullYear()}`;
+
+const fulldate = document.querySelector("#lastModified");
+
+fulldate.innerHTML = `Last Modification : <span class="highlight">${new Intl.DateTimeFormat(
+  "en-US",
+  {
+    dateStyle: "short",
+    timeStyle: "medium",
+  }
+).format(today)}</span>`;
---
+++ chamber/scripts/directory.js
@@ -0,0 +1,40 @@
+let menulist = document.getElementById("menulist");
+let menuIcon = document.querySelector(".menu-icon i");
+
+menulist.style.display = "none";
+
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    const menuIcon = document.querySelector('.menu-toggle');
+    
+    navLinks.classList.toggle('active');
+    
+   
+    if (navLinks.classList.contains('active')) {
+        menuIcon.classList.remove('fa-bars');
+        menuIcon.classList.add('fa-times');
+    } else {
+        menuIcon.classList.remove('fa-times');
+        menuIcon.classList.add('fa-bars');
+    }
+}
+
+
+document.addEventListener('click', (e) => {
+    const navLinks = document.querySelector('.nav-links');
+    const menuIcon = document.querySelector('.menu-icon');
+    
+    if (!menuIcon.contains(e.target) && !navLinks.contains(e.target) && navLinks.classList.contains('active')) {
+        toggleMenu();
+    }
+});
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 768) {
+    menulist.style.display = "flex";
+    menuIcon.classList.remove("fa-times");
+    menuIcon.classList.add("fa-bars");
+  } else {
+    menulist.style.display = "none";
+  }
+});
